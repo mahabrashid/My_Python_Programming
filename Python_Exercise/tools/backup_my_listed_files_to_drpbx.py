e333b851ed818c05272b573bfdcce2a891d48615
Mahabubur Rashid <marashid@checkpoint.com>
2017-12-17 23:06:46 +0000

    enabled logging and improved code to handle timeout as well other previously uncaught exceptions

--- Python_Exercise/tools/backup_my_listed_files_to_drpbx.py
+++ Python_Exercise/tools/backup_my_listed_files_to_drpbx.py
@@ -3,7 +3,7 @@ Created on 2 Dec 2017
 
 @author: marashid
 '''
-import sys
+import os, sys
 from _datetime import datetime, timedelta
 import logging
 
@@ -19,10 +19,21 @@ def _get_file_metadata(file, required_metadata):
     '''
     retuns ONLY the required_metadata for a given file
     
-    @param file: str :the file to enquire a metadata for
-    @param required_metadata :str :metadata required for the program
+    @param file :str -the file to enquire a metadata for
+    @param required_metadata :str -metadata required for the program
     '''
-    loc_file_meta_data = (file_metadata_reader.get_metadata_for_single_file(file))
+    ## only file path is supported at the moment, so directory path will rejected
+    if os.path.isdir(file):
+        logging.error("The path in the csv file, {}, is a directory. Please change this to a file and try again.".format(file))
+        sys.exit("The path in the csv file, {}, is a directory. Please change this to a file and try again.".format(file))
+    
+    try:
+        loc_file_meta_data = (file_metadata_reader.get_metadata_for_single_file(file))
+    except Exception as exp:
+        logging.error("Couldn't get metadata for the local file {} due to following exception: {}".format(file, exp.__str__()))
+        print("Couldn't get metadata for the local file {} due to following exception: {}".format(file, exp.__str__()))
+        return None
+        
 #     print(datetime.fromtimestamp(loc_file_meta_data['modify_time']))
     metadata = datetime.fromtimestamp(loc_file_meta_data[required_metadata])
     return metadata
@@ -31,10 +42,34 @@ def _get_drpbx_file_metadata(file, required_metadata):
     '''
     retuns ONLY the required_metadata for a given file
     
-    @param file: str :the file to enquire a metadata for
-    @param required_metadata :str :metadata required for the program
+    @param file :str -the file to enquire a metadata for
+    @param required_metadata :str -metadata required for the program
     '''
-    drpbx_file_meta_data = dropbox_api_gateway.get_my_drpbx_file_metadata(file)
+#     print("dropbox path: " + file)
+    ## only file path is supported at the moment, so directory path will be rejected
+    if os.path.isdir(file):
+        logging.error("The path in the csv file, {}, is a directory. Please change this to a file and try again.".format(file))
+        sys.exit("The path in the csv file, {}, is a directory. Please change this to a file and try again.".format(file))
+    
+    try:
+        drpbx_file_meta_data = dropbox_api_gateway.get_my_drpbx_file_metadata(file)
+    
+    except ApiError as err:
+        ## if the file doesn't exist on dropbox, we will create the file for first time
+        if(dropbox_api_gateway.no_such_file_on_dropbox(err)):
+            logging.error("specified file is not found on Dropbox, creating dropbox file {}...".format(backup_file))
+            print("specified file is not found on Dropbox, creating dropbox file {}...".format(backup_file))
+            dropbox_api_gateway.backup(local_file, backup_file)
+        else:
+            logging.error("error returned from ApiError: " + err.error.__str__())
+            print("error returned from ApiError: " + err.error.__str__())
+        return None
+    
+    except Exception as exp:
+        logging.error("Couldn't get metadata for the dropbox path {} due to following exception: {}".format(file, exp.__str__()))
+        print("Couldn't get metadata for the dropbox path {} due to following exception: {}".format(file, exp.__str__()))
+        return None
+    
     metadata = drpbx_file_meta_data[required_metadata]
     return metadata
 
@@ -62,13 +97,19 @@ logging.basicConfig(filename=my_logfile_handler.baseFilename, level=logging.DEBU
 ## initiate the dropbox object
 dropbox_api_gateway.initiate_drpbx_obj()
 
-csv_file = "./data_files/files_to_backup_test.csv"
 ## read all Local files and their corresponding dropbox locations from a csv file
+csv_file = "./data_files/files_to_backup.csv"
 try:
-    my_backup_files_list = csv_reader.read_csv(csv_file)
+    my_backup_files_list = csv_reader.read_csv(csv_file)    
 except Exception as err:
-    logging.error(err.strerror + ": " + csv_file)
-    sys.exit((err.strerror + ": " + csv_file))
+    logging.error("Error reading csv file {}. Program is exiting due to the following error: {}".format(csv_file, err.__str__()))
+    sys.exit("Error reading csv file {}. Program is exiting due to the following error: {}".format(csv_file, err.__str__()))
+
+## if any of the key fields is missing in csv file, then program cannot execute further and should terminate
+if ('LocalFile' not in my_backup_files_list.__getitem__(0).keys()) or ('BackUpPath' not in my_backup_files_list.__getitem__(0).keys()):
+    logging.error("Missing one or more required key fields in the csv file, ensure to have 'LocalFile' and 'BackUpPath' keys in the given csv")
+    sys.exit("Missing one or more required key fields in the csv file, ensure to have 'LocalFile' and 'BackUpPath' keys in the given csv")
+
 ## now iterate through each item in the list and perform operations
 for record in my_backup_files_list:
     print("="*50)
@@ -83,26 +124,15 @@ for record in my_backup_files_list:
     loc_file_modified_time = _get_file_metadata(local_file, "modify_time")    ## get the local file's modify_time
     logging.debug("local file modify_time: " + str(loc_file_modified_time))
     print("local file modify_time: " + str(loc_file_modified_time))
-    try:
-        drpbx_file_modified_time = _get_drpbx_file_metadata(backup_file, "modify_time")   ## get the backup file's modify_time
-        logging.debug("dropbox file server_modified time: " + str(drpbx_file_modified_time))
-        print("dropbox file server_modified time: " + str(drpbx_file_modified_time))
-    except ApiError as err:
-        ## if the file doesn't exist on dropbox, we will create the file for first time
-        if(dropbox_api_gateway.no_such_file_on_dropbox(err)):
-            logging.error("specified file is not found on Dropbox, creating dropbox file {}...".format(backup_file))
-            print("specified file is not found on Dropbox, creating dropbox file {}...".format(backup_file))
-            dropbox_api_gateway.backup(local_file, backup_file)
-        else:
-            logging.error("error returned from ApiError: " + err.error)
-            print("error returned from ApiError: " + err.error)
-            logging.error("error message: " + err.user_message_text)
-            print("error message: " + err.user_message_text)
-            sys.exit()
-    ## if any othe error occurs, system should exit with an error
-    except Exception as exp:
-        sys.exit("program is exiting due to unknown exception...")
+    if loc_file_modified_time is None:
+        continue    ## do not execute rest of the code, go to the next record
     
+    drpbx_file_modified_time = _get_drpbx_file_metadata(backup_file, "modify_time")   ## get the backup file's modify_time
+    logging.debug("dropbox file server_modified time: " + str(drpbx_file_modified_time))
+    print("dropbox file server_modified time: " + str(drpbx_file_modified_time))
+    if drpbx_file_modified_time is None:
+        continue    ## do not execute rest of the code, go to the next record
+           
     ## find out whether local file was modified since the server's last update, or vice-versa ignoring the last 5 hours update on dropbox
     leading_time = _get_leading_time(loc_file_modified_time, drpbx_file_modified_time)
     if(leading_time == loc_file_modified_time):
@@ -110,22 +140,20 @@ for record in my_backup_files_list:
         print("local file is ahead of server, uploading latest local file to dropbox...")
         try:
             dropbox_api_gateway.backup(local_file, backup_file)
+        
         except ApiError as err:
-            if(dropbox_api_gateway.no_such_file_on_dropbox(err)):
-                logging.error("specified file is not found on Dropbox, check the file name and path are correct.")
-                print("specified file is not found on Dropbox, check the file name and path are correct.")
-        except ApiError as err:
-            logging.error("error returned from ApiError: " + err.error)
-            print("error returned from ApiError: " + err.error)
-            logging.error("error message: " + err.user_message_text)
-            print("error message: " + err.user_message_text)
+            if err.user_message_text:
+                logging.error(err.user_message_text + ", cannot backup {} to {}".format(local_file, backup_file))
+                print(err.user_message_text + ", cannot backup {} to {}".format(local_file, backup_file))
+
         except Exception as exp:
-            logging.error("program is exiting due to unknown exception...")    
-            sys.exit("program is exiting due to unknown exception...")    
+            logging.error(exp.__str__() + ", cannot backup {} to {}".format(local_file, backup_file))    
+            print(exp.__str__() + ", cannot backup {} to {}".format(local_file, backup_file))    
     
     elif(leading_time == drpbx_file_modified_time):
         logging.info("dropbox file is ahead of the local copy, downloading latest file from dropbox...(***yet to implement***)")
         print("dropbox file is ahead of the local copy, downloading latest file from dropbox...(***yet to implement***)")
+    
     else:
         logging.info("both files are same (ignoring updates in last 5 hours). No upload/download has taken place.")
         print("both files are same (ignoring updates in last 5 hours). No upload/download has taken place.")
--- Python_Exercise/tools/dropbox_api_gateway.py
+++ Python_Exercise/tools/dropbox_api_gateway.py
@@ -23,11 +23,35 @@ import logging
 ACCESS_TOKEN = None
 dbx = None
 
-# Uploads contents of LOCALFILE to Dropbox
+
+def no_such_file_on_dropbox(err):
+    '''
+    return True/False for a file's existence on dropbox
+    @param err :ApiError object
+    '''    
+## following lines were important to experiment how to narrow down to the most specific ApiError in order to take appropriate decision
+#     print(err.error)
+#     print(type(str(err.error)))         
+#     print("LookupError('not_found'" in (str(err.error)))
+#     print(dir(err))
+#     print(err.__dict__['error'])
+#     print(err.__dict__['error'].__class__.__dict__)
+#     print(dir(err.__dict__['error'].__dict__['path']))
+#     print(err.__dict__['error'].__class__.__dict__['path'].__dict__)
+#     print("specified file is not found on Dropbox, check the file name and path are correct.")
+    return True if("LookupError('not_found'" in (str(err.error))) else False
+
+
 def backup(LOCALFILE, BACKUPPATH):  ##### modified to include the parameters    #####
+    '''
+    Uploads contents of LOCALFILE to Dropbox
+    
+    While inevitably terminating exceptions be caught here and program killed, other exceptions should be passed to the caller to decide subsequent actions.
+    '''
     if(LOCALFILE is (None or "") or BACKUPPATH is (None or "")):
-        logging.error("exiting system due to invalid parameters, please try again with valid LOCALFILE and BACKPATH")
-        sys.exit("exiting system due to invalid parameters, please try again with valid LOCALFILE and BACKPATH")
+        logging.error("LOCALFILE or BACKUP path is invalid, cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
+        print("LOCALFILE or BACKUP path is invalid, cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
+        return
     
     ##### modified to include invalid object check    #####
     if(dbx is None):
@@ -48,14 +72,22 @@ def backup(LOCALFILE, BACKUPPATH):  ##### modified to include the parameters    
                     err.error.get_path().reason.is_insufficient_space()):
                 logging.error("ERROR: Cannot back up; insufficient space.")
                 sys.exit("ERROR: Cannot back up; insufficient space.")
-            elif err.user_message_text:
-                logging.error(err.user_message_text)
-                print(err.user_message_text)
-                sys.exit()
-            else:
-                logging.error(err)
-                print(err)
-                sys.exit()
+        except Exception as exp:
+            if ("The write operation timed out" in (str(exp.__str__())) and (dbx._timeout < 120)):    ## if a timeout occurs, increase the _timeout by 30 seconds and try again until _timeout value reaches 2 minutes
+                logging.debug("Timeout occurred during write operation, increasing the timeout by 30 seconds...")
+                print("Timeout occurred during write operation, increasing the timeout by 30 seconds...")
+                dbx._timeout += 30
+                logging.debug("New timeout value is: " + dbx._timeout.__str__())
+                print("New timeout value is: " + dbx._timeout.__str__())
+                backup(LOCALFILE, BACKUPPATH)
+                
+            ## following are some example of how an exception thrown from this method call can be handled by the caller
+#             elif err.user_message_text:
+#                 logging.error(err.user_message_text + "cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
+#                 print(err.user_message_text + "cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
+#             else:
+#                 logging.error(err.__str__() + "cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
+#                 print(err.__str__() + "cannot backup {} to {}".format(LOCALFILE, BACKUPPATH))
         
 '''
 # Change the text string in LOCALFILE to be new_content
@@ -98,11 +130,14 @@ def get_my_drpbx_file_metadata(dropbox_file_path):
     '''
     Get the metadata for a given file in dropbox
     example metadata: FileMetadata(name='my cv23.pdf', id='id:CJ_idZ9Oln4AAAAAAAABow', client_modified=datetime.datetime(2014, 5, 14, 22, 16, 29), server_modified=datetime.datetime(2014, 5, 14, 22, 16, 33), rev='2f0d02782059', size=98739, path_lower='/cv stuff/tailored cvs/my cv23.pdf', path_display='/cv stuff/tailored CVs/my cv23.pdf', parent_shared_folder_id=None, media_info=None, sharing_info=None, property_groups=None, has_explicit_shared_members=None, content_hash='6c250b055fbd5f77cb457e99ee60228e1c6c71e417fc5f6abb24ffcd44d5f644')
+    @param drop_file_path :str -path to file on dropbox
+    
+    While inevitably terminating exceptions be caught here and program killed, other exceptions should be passed to the caller to decide subsequent actions.
     '''
     if(dbx is None):
         logging.error("dropbox object is set to None, please initiate dropbox object first.")
         sys.exit("dropbox object is set to None, please initiate dropbox object first.")
-    
+
     file_metadata = dbx.files_get_metadata(dropbox_file_path)
 #     print(dir(file_metadata))
 #     print(file_metadata.path_display)
@@ -110,60 +145,51 @@ def get_my_drpbx_file_metadata(dropbox_file_path):
 #     print(file_metadata.server_modified)
 #     print(file_metadata.size)
     return {"path":file_metadata.path_display, "modify_time":file_metadata.server_modified, "size":file_metadata.size}
+    
 
 def print_my_drpbx_files():
     '''
     print all file names in the dropbox root directory
+    
+    While inevitably terminating exceptions be caught here and program killed, other exceptions should be passed to the caller to decide subsequent actions.
     '''
     for entry in dbx.files_list_folder('').entries:
         print(entry.name)
 
+
 def initiate_drpbx_obj():
+    '''
+    While inevitably terminating exceptions be caught here and program killed, other exceptions should be passed to the caller to decide subsequent actions.
+    '''
     global ACCESS_TOKEN
     try:
         accesstoken_file = "./data_files/do_not_commit.txt"
         with open(accesstoken_file) as f:
             for line in f:
                 ACCESS_TOKEN = line
-    except Exception as err:
-        logging.error(err.strerror + ": " + accesstoken_file)
-        sys.exit((err.strerror + ": " + accesstoken_file))
-        # Check for an access token
-    if (len(ACCESS_TOKEN) == 0 or ACCESS_TOKEN == '' or ACCESS_TOKEN is None):
-        logging.error("ERROR: Looks like you didn't add your access token. ")
-        sys.exit("ERROR: Looks like you didn't add your access token. ")
-
-    # Create an instance of a Dropbox class, which can make requests to the API.
-    logging.debug("Creating a Dropbox object...")
-    print("Creating a Dropbox object...")
-    global dbx
-    dbx = dropbox.Dropbox(ACCESS_TOKEN)
-
-    # Check that the access token is valid
-    try:
-        dbx.users_get_current_account()
-    except AuthError as err:
-        logging.error("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
-        sys.exit("ERROR: Invalid access token; try re-generating an "
-            "access token from the app console on the web.")
-
     
-def no_such_file_on_dropbox(err):
-    '''
-    return True/False for a file's existence on dropbox
-    @param err: ApiError object
-    '''    
-## following lines were important to experiment how to narrow down to the most specific ApiError in order to take appropriate decision
-#     print(err.error)
-#     print(type(str(err.error)))         
-#     print("LookupError('not_found'" in (str(err.error)))
-#     print(dir(err))
-#     print(err.__dict__['error'])
-#     print(err.__dict__['error'].__class__.__dict__)
-#     print(dir(err.__dict__['error'].__dict__['path']))
-#     print(err.__dict__['error'].__class__.__dict__['path'].__dict__)
-#     print("specified file is not found on Dropbox, check the file name and path are correct.")
-    return True if("LookupError('not_found'" in (str(err.error))) else False
+        # Check for an access token
+        if (len(ACCESS_TOKEN) == 0 or ACCESS_TOKEN == '' or ACCESS_TOKEN is None):
+            raise RuntimeError("ERROR: Looks like you didn't add your access token. ")
+    
+        # Create an instance of a Dropbox class, which can make requests to the API.
+        logging.debug("Creating a Dropbox object...")
+        print("Creating a Dropbox object...")
+        global dbx
+        dbx = dropbox.Dropbox(ACCESS_TOKEN)
+    
+        # Check that the access token is valid        
+        dbx.users_get_current_account()
+        
+    except FileNotFoundError as fnferr:
+        logging.error(fnferr.strerror + ": " + accesstoken_file)
+        sys.exit((fnferr.strerror + ": " + accesstoken_file))
+    except RuntimeError as rterr:
+        logging.error("Program is exiting due to the following error: " + rterr.__str__())
+        sys.exit("Program is exiting due to the following error: " + rterr.__str__())
+    except AuthError:
+        logging.error("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+        sys.exit("ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
 
 ##################################################
 ### end of extended code                     ###
@@ -174,29 +200,28 @@ if __name__ == '__main__':
 ###   extended code to test functionality       ###
 ########################################################
     initiate_drpbx_obj()
-
-    ## Print all dropbox files
-#     print_my_drpbx_files()
-    
-    ## Create a backup of the current settings file
-#     backup(r"C:\Users\marashid\Documents\Personal_Stuff\Personal Training and Development\Python\Python_Exercise\tools\sample_CSVs\SimpleCSVSample.csv", "/SimpleCSVSample.csv")
     
     try:
-        print((get_my_drpbx_file_metadata("/SimpleCSVSample.csv")))
+        ## print metadata for a given dropbox file
+        print((get_my_drpbx_file_metadata("/L&D/AWS CSA/section2_notes.docx")))
+        print((dbx._timeout))
+        
+        ## Print all dropbox files
+#         print_my_drpbx_files()
+    
+        ## Create a backup of the current settings file
+#         backup(r"C:\Users\marashid\Documents\Personal_Stuff\Personal Training and Development\Python\Python_Exercise\tools\sample_CSVs\SimpleCSVSample.csv", "/SimpleCSVSample.csv")
+    
     except ApiError as err:
         if(no_such_file_on_dropbox(err)):
             logging.error("specified file is not found on Dropbox, check the file name and path are correct.")
             print("specified file is not found on Dropbox, check the file name and path are correct.")
     except ApiError as err:
-        logging.error("some ApiError has occurred which excludes 'file not found on dropbox'")
-        print("some ApiError has occurred which excludes 'file not found on dropbox'")
-        logging.error("error returned from ApiError: " + err.error)
-        print("error returned from ApiError: " + err.error)
-        logging.error("error message: " + err.user_message_text)
-        print("error message: " + err.user_message_text)
+        logging.error("error returned from ApiError: " + err.error.__str__())
+        print("error returned from ApiError: " + err.error.__str__())
     except Exception as exp:
-        logging.error("program is exiting due to unknown exception...")
-        sys.exit("program is exiting due to unknown exception...")
+        logging.error("program is exiting due to following exception..." + exp.__str__())
+        sys.exit("program is exiting due to following exception..." + exp.__str__())
 
 ########################################################
 ###   end of extended code                      ###
